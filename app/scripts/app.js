646f81e267d3f73ecccdb41cbc55c1de237354fb
Ady Suprianto <adysuprianto@yahoo.co.id>
2017-02-09 00:03:02 +0700

    new Arr

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,32 +1,34 @@
 'use strict';
+var Config = angular.module('newAarApp.config', []);
 var App = angular
-  .module('newAarApp', [
-    'ui.router',
-    'ui.bootstrap',
-    'ngMaterial',
-    'smart-table',
-    'ui-notification',
-    'ngFileUpload',
-    'naif.base64',
-    'ngResource',
-    'ui.select',
-    'ngScrollbars',
-    'ngLodash',
-    'ngStorage'
-  ])
-  .config(function (ScrollBarsProvider) {
-    // the following settings are defined for all scrollbars unless the
-    // scrollbar has local scope configuration
-    ScrollBarsProvider.defaults = {
-        scrollButtons: {
-            scrollAmount: 'auto', // scroll amount when button pressed
-            enable: true // enable scrolling buttons by default
-        },
-        scrollInertia: 400, // adjust however you want
-        axis: 'yx', // enable 2 axis scrollbars by default,
-        theme: 'minimal-dark',
-        autoHideScrollbar: false
-    };
-    
-    PDFJS.workerSrc = 'root/factories/pdfjs/build/pdf.worker.js';
-});
+    .module('newAarApp', [
+        'ui.router',
+        'ui.bootstrap',
+        'ngMaterial',
+        'smart-table',
+        'ui-notification',
+        'ngFileUpload',
+        'naif.base64',
+        'ngResource',
+        'ui.select',
+        'ngScrollbars',
+        'ngLodash',
+        'ngStorage',
+        'perfect_scrollbar'
+    ])
+    .config(function(ScrollBarsProvider) {
+        // the following settings are defined for all scrollbars unless the
+        // scrollbar has local scope configuration
+        ScrollBarsProvider.defaults = {
+            scrollButtons: {
+                scrollAmount: 'auto', // scroll amount when button pressed
+                enable: true // enable scrolling buttons by default
+            },
+            scrollInertia: 400, // adjust however you want
+            axis: 'yx', // enable 2 axis scrollbars by default,
+            theme: '3d-thick-dark',
+            autoHideScrollbar: false
+        };
+
+        PDFJS.workerSrc = 'root/factories/pdfjs/build/pdf.worker.js';
+    });
